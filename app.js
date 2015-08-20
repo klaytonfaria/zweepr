5b479d6c774cc69b4d1557cccddf282b4310079e
Klayton Faria <klayton.souza@gmail.com>
2015-08-20 20:01:06 -0300

    Adding recursion to the read script

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 "use strict"
 
-var cli = require("cli"),
+var Q = require("q"),
+    cli = require("cli"),
     glob = require("glob"),
     json = require("jsonfile"),
     program = require("commander"),
@@ -13,42 +14,57 @@ var cli = require("cli"),
 
     program
       .version('0.1.0')
-      .option('-i, --init', 'make initial component structure')      
-
-    program.on('--help', function(){
-      console.log('  Examples:');
-      console.log('');
-      console.log('    $ node app --help');
-      console.log('    $ node app -h');
-      console.log('');
-    });
-
-    program.parse(process.argv);
+      .option('-i, --init', 'make initial component structure', makeBoilerplate)
+      .on('--help', function(){
+        console.log('  Examples:');
+        console.log('');
+        console.log('    $ node app --help');
+        console.log('    $ node app -h');
+        console.log('');
+      })
+      .parse(process.argv);
 
 
-
-
-function readJson(filePath) {
-  glob(filePath, function(err, files) {
-    for(var i = 0; i < files.length; i++) {
-      cli.info("Found component: " + files[i]);
-      var obj = json.readFileSync(files[i]),
-          newObject = appSettings()
-                      .createPage(obj)
-                      .addScript(obj)
-                      .getObject();
-
-      output.push(newObject);
-    }
-  });
+function makeBoilerplate() {
+  cli.info("Created initial component structure at: " + process.env.PWD);
+  process.exit(1);
 }
+
+
+function readFile(file) {
+  var deferred = Q.defer(),
+      fileList = glob.sync(file);
+
+      // console.log("--->", fileList);
+
+  for(var i = 0; i < fileList.length; i++) {
+    var obj = json.readFileSync(fileList[i]),
+        resultObj = appSettings()
+                    .createPage(obj)
+                    .addScript(obj),
+        componentList = resultObj.getComponents(obj);
+    cli.info("Component found: " + obj.name);
+
+    if(componentList.length > 0) {
+      for(var i2 = 0; i2 < componentList.length; i2++) {
+        readFile(componentList[i2]);
+      }
+    }
+    output.push(resultObj.getObject());
+  }
+  deferred.resolve(output);
+  return deferred.promise;
+}
+
+
 
 function writeJson(filePath, obj) {
   json.writeFileSync(filePath, obj, {spaces: 2}, function (err) {
-    cli.info(err);
+    cli.error(err);
   })
-  cli.info("creating " + filePath);
+  cli.ok("File created in: " + filePath);
 }
 
-readJson(filePath);
-writeJson(outputFile, output);
+readFile(filePath).then(function(obj){
+  writeJson(outputFile, obj);
+});
