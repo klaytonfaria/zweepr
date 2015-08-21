04a8a84859976be7b76d80d3640ff0a776712b3f
Klayton Faria <klayton.souza@gmail.com>
2015-08-21 18:27:54 -0300

    Update script

--- app.js
+++ app.js
@@ -5,66 +5,67 @@ var Q = require("q"),
     cli = require("cli"),
     glob = require("glob"),
     json = require("jsonfile"),
-    program = require("commander"),
+    extend = require("util")._extend,
+    program = require("./lib/program"),
     filePath = "/app/walmart-frontend/webstore/assets/desktop/template/**/wm-component.json",
     appSettings = require("./lib/AppSettings"),
     outputFile = "/app/walmart-frontend/webstore/settings.json",
-    registeredComponent = [],
+    page,
     output = [];
-
-
-    program
-      .version('0.1.0')
-      .option('-i, --init', 'make initial component structure', makeBoilerplate)
-      .on('--help', function(){
-        console.log('  Examples:');
-        console.log('');
-        console.log('    $ node app --help');
-        console.log('    $ node app -h');
-        console.log('');
-      })
-      .parse(process.argv);
-
-
-function makeBoilerplate() {
-  cli.info("Created initial component structure at: " + process.env.PWD);
-  process.exit(1);
-}
-
 
 function readFile(file) {
   var deferred = Q.defer(),
       fileList = glob.sync(file);
 
-
-      // console.log("--->", fileList);
-
   for(var i = 0; i < fileList.length; i++) {
     var obj = json.readFileSync(fileList[i]),
-        componentName = obj.name.toLowerCase();
+        componentName = obj.name.toLowerCase(),
+        componentType = obj.type || "";
 
-    // Exclude all duplicated components
-    if(registeredComponent.indexOf(componentName) < 0) {
-      var resultObj = appSettings().createPage(obj).addScript(obj),
-          componentList = resultObj.getComponents(obj);
-
-      cli.info("Component found: " + componentName);
-      registeredComponent.push(componentName);
-
-      if(componentList.length > 0) {
-        for(var i2 = 0; i2 < componentList.length; i2++) {
-          readFile(componentList[i2]);
-        }
-      }
-
-      output.push(resultObj.getObject());
+    if(componentType.toLowerCase() === "page") {
+      page = componentName;
     }
+
+    var resultObj = appSettings().setPageName(page).createPage(obj).addScript(obj),
+        componentList = resultObj.getComponents(obj);
+
+    cli.info("└── Component found: " + componentName);
+
+    if(componentList.length > 0) {
+      for(var i2 = 0; i2 < componentList.length; i2++) {
+        readFile(componentList[i2]);
+      }
+    }
+
+    output.push(resultObj.getObject());
   }
+
   deferred.resolve(output);
   return deferred.promise;
 }
 
 
+function convertListToObject(list) {
+  var obj = {};
+
+  for(var i = 0; i < list.length; i++) {
+    // console.log(list[i]);
+    var key = "key" + i;
+    var pageKey = Object.keys(list[i])[0];
+
+    obj[pageKey] = obj[pageKey] || {};
+    if(list[i][pageKey].scripts) {
+      obj[pageKey].scripts = obj[pageKey].scripts || [];
+      obj[pageKey].scripts = obj[pageKey].scripts.concat(list[i][pageKey].scripts).reverse();
+    }
+    if(list[i][pageKey].styles) {
+      obj[pageKey].styles = obj[pageKey].styles || [];
+      obj[pageKey].styles = obj[pageKey].styles.concat(list[i][pageKey].styles).reverse();
+    }
+  }
+
+  return obj;
+}
 
 function writeJson(filePath, obj) {
   json.writeFileSync(filePath, obj, {spaces: 2}, function (err) {
@@ -73,6 +74,12 @@ function writeJson(filePath, obj) {
   cli.ok("File created in: " + filePath);
 }
 
-readFile(filePath).then(function(obj){
-  writeJson(outputFile, obj);
+function makeBoilerplate() {
+  cli.info("Created initial component structure at: " + process.env.PWD);
+  process.exit(1);
+}
+
+
+readFile(filePath).then(function(obj) {
+  writeJson(outputFile, convertListToObject(obj));
 });
