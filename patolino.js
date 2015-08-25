2481198669838e283a00d905b9fcc66751e8928a
Klayton Faria <klayton.souza@gmail.com>
2015-08-25 16:07:58 -0300

    Add usage helper

--- patolino.js
+++ patolino.js
@@ -1,4 +1,4 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 "use strict"
 
 var cli = require("cli"),
@@ -7,15 +7,11 @@ var cli = require("cli"),
     options = cli.parse(helpme),
     settingsGenerator = require("./lib/settingsGenerator")();
 
-cli.main(function(args, options) {  
+
+cli.main(function(args, options) {
   settingsGenerator.readFile(options.cwd + options.src).then(function(obj) {
     utils.writeJson(options.dist, obj);
   }, function(err) {
     cli.fatal(err);
   });
 });
-
-function makeBoilerplate() {
-  cli.info("Created initial component structure at: " + process.env.PWD);
-  process.exit(1);
-}
