d93bbd10ec8f6e3416e6646e11c9d382785cd5fa
Klayton Faria <klayton.souza@gmail.com>
2015-09-10 21:33:43 -0300

    Add version option

--- patolino.js
+++ patolino.js
@@ -3,11 +3,17 @@
 
 var cli = require("cli"),
     utils = require("./lib/utils"),
+    json = require("jsonfile"),
     helpme = require("./lib/helpme"),
-    options = cli.parse(helpme);
+    options = cli.parse(helpme),
+    pkg = json.readFileSync("package.json", {throws : true}) || null;
 
 cli.main(function(args, options) {
-  var settingsGenerator = require("./lib/settingsGenerator")(options),
-      content = settingsGenerator.readFile(options.cwd + options.src);
-  utils.writeJson(options.dist, content);
+  if(options.version) {
+    console.log(pkg.version);
+  } else {
+    var settingsGenerator = require("./lib/settingsGenerator")(options),
+        content = settingsGenerator.readFile(options.cwd + options.src);
+    utils.writeJson(options.dist, content);
+  }
 });
